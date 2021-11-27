03461c4761e5e5e7f555bd1c5dd0a53f69ab0cd4
Pop-Raul <93659310+Pop-Raul@users.noreply.github.com>
2021-11-27 16:59:17 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// alert('Hello world!');
+// document.write('Hello world');
+console.log('Hello world!');
+
+var myName = "Raul";
+var b = "hello";
+
+console.log(b + ' ' + myName);
+
+for (var i =0; i <= 10; i++){
+    console.log(i);
+}
+i = 0;
+while (i<=10){
+console.log(i);
+   i++;
+}
+
+var ar = [4, 7, true, 'vector', 79]
+console.log(ar[2]);
+console.log(ar.length);
+
+ar.push(5);
+console.log(ar);
+
+ar.pop();
+console.log(ar);
+
+function suma(x,y){
+    var s;
+    s = x + y;
+    return s;
+}
+
+a = 6;
+b = 9;
+
+console.log(suma(a, b));
